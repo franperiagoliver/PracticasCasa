4e779784646767178ed1b45772262f275e32d5c4
franperiagoliver <franperiagoliver@gmail.com>
2017-07-21 16:39:27 +0200

    commit

--- FirstSteps/src/com/house/study/java_/swim/layouts/Calculator.java
+++ FirstSteps/src/com/house/study/java_/swim/layouts/Calculator.java
@@ -11,47 +11,47 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		FrameCalculator frameCalculator = new FrameCalculator();
-		
+
 		frameCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		frameCalculator.setVisible(true);
 	}
 
 }
 
 class FrameCalculator extends JFrame {
-	
+
 	public FrameCalculator() {
-		
+
 		setTitle("Calculator");
 		setBounds(500, 300, 450, 300);
-		
+
 		PanelCalculator panelCalculator = new PanelCalculator();
-		
+
 		add(panelCalculator);
 	}
-	
+
 }
 
 class PanelCalculator extends JPanel {
-	
+
 	private JPanel panelCalculator2;
-	
+
 	public PanelCalculator() {
-		
+
 		setLayout(new BorderLayout());
-		
+
 		JButton screen = new JButton("0");
 		screen.setEnabled(false);
-		
+
 		add(screen, BorderLayout.NORTH);
-		
+
 		panelCalculator2 = new JPanel();
-		
+
 		panelCalculator2.setLayout(new GridLayout(4, 4));
-		
+
 		putButton("7");
 		putButton("8");
 		putButton("9");
@@ -68,19 +68,17 @@ class PanelCalculator extends JPanel {
 		putButton(".");
 		putButton("=");
 		putButton("+");
-		
-		
+
 		add(panelCalculator2, BorderLayout.CENTER);
-		
-		
+
 	}
-	
+
 	private void putButton(String nameButton) {
-		
+
 		JButton button = new JButton(nameButton);
-		
+
 		panelCalculator2.add(button);
-		
+
 	}
-	
+
 }
